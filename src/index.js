84a88d1aa8a45ff2a592f5630853627748141686
Pavel Logunov <paveloguniv@gmail.com>
2023-01-27 21:03:41 +0900

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+     let subStr = '';
+  let str = '';
+  
+    for (let i = 0; i < expr.length; i += 10) {
+     let letterCode = '';
+     subStr = expr.slice(i, i + 10);
+     
+      if (subStr == '**********') {
+        str += ' ';
+      } else {
+        
+        for (let k = 0; k < 9; k += 2) {
+          let bit = subStr.slice(k, k + 2)
+               
+          switch(bit) {
+            case '10':
+              letterCode += '.';
+              break;
+            case '11':
+              letterCode += '-';
+              break;
+            case '00':
+               letterCode += '';
+              break;
+          }
+        } 
+         for (let key in MORSE_TABLE) {
+          if (key == letterCode) {
+            str += MORSE_TABLE[key]
+          } 
+        }
+      } 
+    } return str; 
 }
 
 module.exports = {
